96c530f81f495e596cf4b383af772bc71ed1cbcb
umarfarooq63114 <39738247+umarfarooq63114@users.noreply.github.com>
2018-07-17 23:58:01 +0500

    ye lo bhao

--- app/src/main/java/com/codingdemos/tablayout/MainActivity.java
+++ app/src/main/java/com/codingdemos/tablayout/MainActivity.java
@@ -1,3 +1,94 @@
+//package com.codingdemos.tablayout;
+//
+//import android.os.Build;
+//import android.os.Bundle;
+//import android.support.design.widget.TabItem;
+//import android.support.design.widget.TabLayout;
+//import android.support.v4.content.ContextCompat;
+//import android.support.v4.view.ViewPager;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.util.Log;
+//import android.widget.Toast;
+//
+//import com.pusher.client.Pusher;
+//import com.pusher.client.PusherOptions;
+//import com.pusher.client.channel.Channel;
+//import com.pusher.client.channel.SubscriptionEventListener;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    private static final String TAG = "MTAG";
+//    Toolbar toolbar;
+//    TabLayout tabLayout;
+//    ViewPager viewPager;
+//    PageAdapter pageAdapter;
+//    TabItem tabChats;
+//    TabItem tabStatus;
+//    TabItem tabCalls;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        toolbar = findViewById(R.id.toolbar);
+//        toolbar.setTitle(getResources().getString(R.string.app_name));
+//        setSupportActionBar(toolbar);
+//
+//        tabLayout = findViewById(R.id.tablayout);
+//        tabChats = findViewById(R.id.tabChats);
+//        tabStatus = findViewById(R.id.tabStatus);
+//
+//        viewPager = findViewById(R.id.viewPager);
+//
+//        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+//        viewPager.setAdapter(pageAdapter);
+//
+//
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//                if (tab.getPosition() == 1) {
+//
+//                } else if (tab.getPosition() == 2)
+//
+//                {
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//
+//        PusherOptions options = new PusherOptions();
+//        options.setCluster("ap2");
+//        Pusher pusher = new Pusher("d994b17cd676131c6cb2", options);
+//
+//        Channel channel = pusher.subscribe("sFinder.435");
+//
+//        channel.bind("info", new SubscriptionEventListener() {
+//            @Override
+//            public void onEvent(String channelName, String eventName, final String data) {
+//                Log.d(TAG,data);
+//            }
+//        });
+//
+//        pusher.connect();
+//
+//    }
+//}
+
 package com.codingdemos.tablayout;
 
 import android.os.Build;
@@ -8,9 +99,17 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.SubscriptionEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MTAG";
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
@@ -62,6 +161,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        PusherOptions options = new PusherOptions();
+        options.setCluster("ap2");
+        Pusher pusher = new Pusher("d994b17cd676131c6cb2", options);
+
+        Channel channel = pusher.subscribe("sFinder.435");
+
+        channel.bind("info", new SubscriptionEventListener() {
+            @Override
+            public void onEvent(String channelName, String eventName, final String data) {
+                Log.d(TAG,data);
+            }
+        });
+
+        pusher.connect();
 
     }
 }
---
+++ app/src/main/java/com/codingdemos/tablayout/Model/Technician.java
@@ -0,0 +1,109 @@
+package com.codingdemos.tablayout.Model;
+
+import com.google.gson.annotations.SerializedName;
+public class Technician {
+    @SerializedName("t_name")
+    String t_name;
+    @SerializedName("id")
+    int id;
+    @SerializedName("cnic")
+    String cnic;
+    @SerializedName("email")
+    String email;
+    @SerializedName("address")
+    String address;
+    @SerializedName("status")
+    int status;
+    @SerializedName("phone")
+    String phone;
+    @SerializedName("experience")
+    String experience;
+    @SerializedName("rating")
+    double rating;
+
+    @SerializedName("specaility_id")
+    int specaility_id;
+    @SerializedName("password")
+    String password;
+    @SerializedName("image")
+    String image;
+    @SerializedName("name")
+    String name;
+
+    public Technician(String t_name, String cnic, String address,
+                      String email,String password, int status, String phone,
+                      String experience, double rating, int specaility_id,String image) {
+        this.t_name = t_name;
+        this.cnic = cnic;
+        this.address = address;
+        this.email = email;
+        this.password=password;
+        this.status = status;
+        this.phone = phone;
+        this.experience = experience;
+        this.rating = rating;
+        this.image=image;
+
+        this.specaility_id = specaility_id;
+    }
+
+    public String getName() {
+        return t_name;
+    }
+    public String getSpecName() {
+        return name;
+    }
+    public String getCnic() {
+        return cnic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getPassword() { return password; }
+
+    public int getSpeciality() {
+        return specaility_id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
